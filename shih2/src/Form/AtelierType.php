eb2b5624e4093a3390a5f3be49772d6eff3f7a11
omarhamdi27 <80309615+omarhamdi27@users.noreply.github.com>
2024-04-15 12:47:48 +0100

    trouchiiik

--- shih2/src/Form/AtelierType.php
+++ shih2/src/Form/AtelierType.php
@@ -3,20 +3,51 @@
 namespace App\Form;
 
 use App\Entity\Atelier;
+
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\GreaterThan;
+use Symfony\Component\Validator\Constraints\LessThan;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints as Assert;
 
-class AtelierType extends AbstractType
+class Ateliertype extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('datedebutAtelier')
-            ->add('datefinAtelier')
-            ->add('lienAtelier')
-        ;
+        ->add('datedebutAtelier', DateType::class, [
+            'widget' => 'single_text',
+            'constraints' => [
+                new NotBlank(['message' => 'La date de début atelier  ne peut pas être vide']),
+            ],
+            'required' => true,
+        ])
+        ->add('datefinAtelier', DateType::class, [
+            'widget' => 'single_text',
+            'constraints' => [
+                new NotBlank(['message' => 'La date de fin  atelier ne peut pas être vide']),
+                new GreaterThan([
+                    'propertyPath' => 'parent.data.datedebutAtelier',
+                    'message' => 'La date de fin doit être ultérieure à la date de début.',
+                ]),
+            ],
+            'required' => true,
+        ])
+        ->add('lienAtelier', TextType::class, [
+            'attr' => ['class' => 'form-control', 'placeholder' => 'lienAtelier']
+                // Autres contraintes éventuelles pour le champ lienAtelier
+            ]);
     }
+
+    // Autres méthodes de configuration si nécessaire
+
+
+
+
 
     public function configureOptions(OptionsResolver $resolver): void
     {
--- shih2/src/Form/Cours1Type.php
+++ shih2/src/Form/Cours1Type.php
@@ -6,34 +6,56 @@ use App\Entity\Cours;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\GreaterThan;
+use Symfony\Component\Validator\Constraints\LessThanOrEqual;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class Cours1Type extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('titreCours')
-            ->add('descriCours')
-            ->add('datedebutCours')
-            ->add('datefinCours')
+            ->add('titreCours', TextType::class, [
+                'attr' => ['class' => 'form-control', 'placeholder' => 'Titre du cours'],
+                'constraints' => [
+                    new Regex([
+                        'pattern' => '/^[a-zA-Z]+$/',
+                        'message' => 'Le titre du cours doit contenir uniquement des lettres de l\'alphabet.'
+                    ]),
+                ],
+            ])
+            ->add('descriCours', TextType::class, [
+                'attr' => ['class' => 'form-control', 'placeholder' => 'Description du cours']
+            ])
+            ->add('datedebutCours', DateType::class, [
+                'widget' => 'single_text',
+                'constraints' => [
+                    new NotBlank(['message' => 'La date de début du cours ne peut pas être vide']),
+                ],
+                'required' => true,
+            ])
+            ->add('datefinCours', DateType::class, [
+                'widget' => 'single_text',
+                'constraints' => [
+                    new NotBlank(['message' => 'La date de fin du cours ne peut pas être vide']),
+                    new GreaterThan([
+                        'propertyPath' => 'parent.data.datedebutCours',
+                        'message' => 'La date de fin doit être ultérieure à la date de début.',
+                    ]),
+                ],
+                'required' => true,
+            ])
             ->add('user', EntityType::class, [
                 'class' => User::class,
-                'choice_label' => 'idUser', // Remplacez 'username' par la propriété appropriée de l'utilisateur à afficher dans le formulaire
-                
+                'choice_label' => 'idUser',
+                'attr' => ['class' => 'form-control']
             ]);
-           /* ->add('idUser', EntityType::class, [
-                // looks for choices from this entity
-                'class' => User::class,
-                // uses the User.username property as the visible option string
-                'choice_label' => 'id',
-                // used to render a select box, check boxes or radios
-                'multiple' => false,
-                'expanded' => false,
-            ])*/
-
-        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
